1e146d915a063de08e16d1ccb1f25191c8aacdfa
Matthew English <Matthew.english@softwire.com>
2019-11-18 10:12:17 +0000

    Part 3

--- Email Extraction/Program.cs
+++ Email Extraction/Program.cs
@@ -16,6 +16,7 @@ namespace Email_Extraction
             {
                 Part1(fileText);
                 Part2(fileText);
+                Part3(fileText);
             }
         }
 
@@ -51,12 +52,39 @@ namespace Email_Extraction
             }
             Console.WriteLine($"Part 1\nNumber of substrings matching {softwireDomain}:\t{matchCount}\n");
         }
+
+
         private static void Part2(string fileText)
         {
             const string pattern = @"\b\S+@softwire\.com\b";
             var rgx = new Regex(pattern, RegexOptions.IgnoreCase);
             var validEmails = ExtractValidEmailMatchesFromFile(rgx, fileText);
             Console.WriteLine($"Part 2\nNumber of Softwire emails found using regular expressions:\t{validEmails.Count()}\n");
+        }
+
+        private static void Part3(string fileText)
+        {
+            var domainCounts = new Dictionary<string, int>();
+            // This pattern will incorrectly match email address with a whitespace or an @ symbol in the local part.
+            const string pattern = @"\b\S+@(\S*)\b";
+            var rgx = new Regex(pattern);
+            var validMatches = ExtractValidEmailMatchesFromFile(rgx, fileText);
+
+            foreach (var email in validMatches)
+            {
+                var domain = email.Groups[1].Value;
+                domainCounts.TryGetValue(domain, out var currentCount);
+                domainCounts[domain] = currentCount + 1;
+            }
+
+            var orderedDomains = domainCounts.OrderByDescending(d => d.Value);
+
+            Console.WriteLine("Part 3");
+            Console.WriteLine("{0,-20}{1,-20}", "Domain", "Count");
+            foreach (var domainCount in orderedDomains)
+            {
+                Console.WriteLine("{0,-20}{1,-20}", domainCount.Key, domainCount.Value);
+            }
         }
 
         private static IEnumerable<Match> ExtractValidEmailMatchesFromFile(Regex regex, string fileText)
@@ -69,9 +97,9 @@ namespace Email_Extraction
         private static bool IsValidEmail(string email)
         {
             return Regex.IsMatch(email,
-                @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
-                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-0-9a-z]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",
-                RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+                    @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                    @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-0-9a-z]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",
+                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
         }
     }
 }
